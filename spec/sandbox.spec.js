37d9450b51f5d7762acfba77853b9ac21dd899eb
Girma Nigusse <xgirma@gmail.com>
2020-05-14 22:32:48 -0400

    updates

--- spec/sandbox.spec.js
+++ spec/sandbox.spec.js
@@ -25,7 +25,7 @@ describe("Sandbox", () => {
   });
 
   it("should be on sandbox", async () => {
-    await page.waitFor("h1");
+    await page.waitForSelector("h1");
     const title = await page.$eval("h1", (el) => el.textContent);
 
     expect(await page.title()).to.equal("Sandbox");
